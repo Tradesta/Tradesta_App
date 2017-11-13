ae11bbbfba174c04dc63887f425a547a3e50781d
Tay Mackay <tay.mackay@hotmail.com>
2017-11-13 22:43:28 +1030

    GAuthSecKey DB Mig

---
+++ Tradesta/Migrations/201711131212339_GoogleAuthenticatorSecretKey.cs
@@ -0,0 +1,16 @@
+namespace Tradesta.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class GoogleAuthenticatorSecretKey : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
